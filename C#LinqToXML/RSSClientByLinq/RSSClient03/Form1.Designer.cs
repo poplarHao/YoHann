85f8fbf172c5ddb82045a8feba2a0b928d1c1218
poplarHao <771378619@qq.com>
2018-03-30 09:34:18 +0800

    利用LinqTOXML读取新闻

---
+++ C#LinqToXML/RSSClientByLinq/RSSClient03/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace RSSClient03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_localnews = new System.Windows.Forms.Button();
+            this.listBox_mynews = new System.Windows.Forms.ListBox();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.btn_del = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_localnews
+            // 
+            this.btn_localnews.Location = new System.Drawing.Point(1, 0);
+            this.btn_localnews.Name = "btn_localnews";
+            this.btn_localnews.Size = new System.Drawing.Size(268, 36);
+            this.btn_localnews.TabIndex = 0;
+            this.btn_localnews.Text = "我的订阅";
+            this.btn_localnews.UseVisualStyleBackColor = true;
+            this.btn_localnews.Click += new System.EventHandler(this.btn_localnews_Click);
+            // 
+            // listBox_mynews
+            // 
+            this.listBox_mynews.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox_mynews.BackColor = System.Drawing.SystemColors.Info;
+            this.listBox_mynews.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox_mynews.FormattingEnabled = true;
+            this.listBox_mynews.ItemHeight = 17;
+            this.listBox_mynews.Location = new System.Drawing.Point(3, 42);
+            this.listBox_mynews.Name = "listBox_mynews";
+            this.listBox_mynews.Size = new System.Drawing.Size(266, 208);
+            this.listBox_mynews.TabIndex = 1;
+            this.listBox_mynews.SelectedIndexChanged += new System.EventHandler(this.listBox_mynews_SelectedIndexChanged);
+            this.listBox_mynews.DoubleClick += new System.EventHandler(this.listBox_mynews_DoubleClick);
+            this.listBox_mynews.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox_mynews_MouseDoubleClick);
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.richTextBox.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox.Location = new System.Drawing.Point(275, 42);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(700, 208);
+            this.richTextBox.TabIndex = 2;
+            this.richTextBox.Text = "";
+            this.richTextBox.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.richTextBox_LinkClicked);
+            // 
+            // btn_del
+            // 
+            this.btn_del.Location = new System.Drawing.Point(27, 255);
+            this.btn_del.Name = "btn_del";
+            this.btn_del.Size = new System.Drawing.Size(82, 37);
+            this.btn_del.TabIndex = 3;
+            this.btn_del.Text = "删除订阅";
+            this.btn_del.UseVisualStyleBackColor = true;
+            this.btn_del.Click += new System.EventHandler(this.btn_del_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(404, 265);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(367, 21);
+            this.textBox1.TabIndex = 4;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(124, 255);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(74, 36);
+            this.btn_add.TabIndex = 5;
+            this.btn_add.Text = "添加订阅";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(228, 268);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "标题";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(369, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "网址";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(263, 265);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 8;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(7, 292);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(967, 435);
+            this.webBrowser1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(402, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(281, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "注意：选中标题进行删除。双击链接打开此分类页面";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 741);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_del);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.listBox_mynews);
+            this.Controls.Add(this.btn_localnews);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "第三组RSS新闻订阅";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_localnews;
+        private System.Windows.Forms.ListBox listBox_mynews;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Button btn_del;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ JQUERYdemo/caidan/jQuery.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $(".main > a").next("ul").hide();//使得加载页面看不见子菜单 选择器含义：选择main类下儿子一代中的<a>标记
+    $(".hmain > a").next("ul").hide();//使得加载页面看不见子菜单
+    $(".main > a").click(function () {
+        var ulNode = $(this).next("ul");
+        if (ulNode.css("display") == "none") {
+            ulNode.slideDown();
+            $(':focus').css("background-image", "url('001.jpg')");//改变背景图片
+        } else {
+            ulNode.slideUp();
+            $(':focus').css("background-image", "url('002.jpg')");//改变背景图片
+        }
+    });
+    $(".hmain").hover(function () {
+        $(this).children("ul").stop();//停止以前未执行的动画，直接执行下面动画
+        $(this).children("ul").slideDown();
+        $(this).children("a").css("background-image", "url('001.jpg')");//改变背景图片
+    }, function () {
+        $(this).children("ul").stop();//停止以前未执行的动画，直接执行下面动画
+        $(this).children("ul").slideUp();
+        $(this).children("a").css("background-image", "url('002.jpg')");//改变背景图片
+    })
+})
